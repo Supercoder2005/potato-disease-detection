782e8c7da47b611f81d4e19629241e8a18786ca5
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2026-04-01 15:57:41 +0000

    update

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,File,UploadFile
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive"
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,host='localhost',port=8000)
+
